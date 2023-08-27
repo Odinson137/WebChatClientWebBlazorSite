596a36766239c1944270f823e4bc0f58e30451c2
Yura <you@example.com>
2023-08-27 22:29:55 +0300

    add login and registr page

---
+++ Data/AppSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace WebChatClientWebSite.Data
+{
+    public static class AppSettings
+    {
+        public static string ApiUrl { get; } = "http://localhost:5000";
+        //public static string ApiUrl { get; set; } = "https://localhost:7078";
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace WebChatClientWebSite.Models
+{
+    public class User
+    {
+        public string Id { get; set; } = null!;
+        public string UserName { get; set; } = null!;
+    }
+
+    public class CreateUser
+    {
+        public string UserName { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+}
