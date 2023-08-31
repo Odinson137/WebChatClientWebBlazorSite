c84f0d57fe320fa022b43ce2260eeef5bb7ff253
Yura <you@example.com>
2023-08-31 14:17:26 +0300

    add TextBoxComponents, add sendmessage, update design

--- Models/Message.cs
+++ Models/Message.cs
@@ -7,6 +7,6 @@ namespace WebChatClientWebSite.Models
         public int ChatId { get; set; }
         public string Id { get; set; } = null!;
         public DateTime SendTime { get; set; }
-        public string text { get; set; } = null!;
+        public string Text { get; set; } = null!;
     }
 }
--- wwwroot/scripts/chatList.js
+++ wwwroot/scripts/chatList.js
@@ -1,7 +1,14 @@
-function scrollToBottom(element) {
-    console.log(element);
-    const lastChild = element.lastElementChild;
-    if (lastChild) {
-        lastChild.scrollIntoView({ behavior: "smooth" });
-    }
+function scrollToBottom(targetNode) {
+    const config = { attributes: false, childList: true, subtree: false };
+    const callback = function (mutationsList, observer) {
+        targetNode.scrollTop = targetNode.scrollHeight
+    };
+
+    const observer = new MutationObserver(callback);
+    observer.observe(targetNode, config);
+}
+
+function autoResize(textarea) {
+    textarea.style.height = 'auto';
+    textarea.style.height = (textarea.scrollHeight) + 'px';
 }
